e9a66ea90ed973a4abe82f97bf358e990ace0ce2
meet <smilehackerofficial@gmail.com>
2020-12-09 16:07:02 +0530

    s

--- payload.js
+++
@@ -1,5 +0,0 @@
-script = document.createElement('script');
-script.setAttribute('src', 'alert.js');
-script.setAttribute('crossorigin', 'anonymous');
-window.top.document.getElementsByTagName('iframe')[0].contentDocument.body.appendChild(script);
-    
